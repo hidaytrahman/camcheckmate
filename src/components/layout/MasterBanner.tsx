284f5c8f91eefec616d4e616a142013287d65efd
Hidayt Rahman <hidaytrahman@gmail.com>
2023-09-08 23:07:05 +0530

    chore: code refactor

--- src/components/layout/MasterBanner.tsx
+++ src/components/layout/MasterBanner.tsx
@@ -92,13 +92,6 @@ export default function MasterBanner() {
 						<Fade delay={4000}>
 							<div className='mt-10 flex items-center justify-center gap-x-6'>
 								<AttentionSeeker effect='heartBeat' delay={9000}>
-									{/* <a
-                                        href="#"
-                                        className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                                    >
-                                        Get started
-                                    </a> */}
-
 									<Button size='xlarge' onClick={() => navigate('/camera')}>
 										Get started
 									</Button>
