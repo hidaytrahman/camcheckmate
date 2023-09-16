4b4be3f99ff5d7f79a06bd3d9e0186e79cbe4019
Hidayt Rahman <hidaytrahman@gmail.com>
2023-09-16 18:37:11 +0530

    refactor: code

--- src/components/features/WebCamera.tsx
+++ src/components/features/WebCamera.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import Dropdown from '../common/Dropdown';
 import Webcam from 'react-webcam';
 
