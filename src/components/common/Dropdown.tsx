04e709608af11a4502833ada38a6eecc83675aa7
Hidayt Rahman <hidaytrahman@gmail.com>
2024-02-12 01:42:13 +0530

    feat: default selection

--- src/components/common/Dropdown.tsx
+++ src/components/common/Dropdown.tsx
@@ -11,11 +11,14 @@ function classNames(...classes) {
 type DropdownPropsTypes = { title: string; items: []; onChangeAction: (arg: any) => void; activeDevice: any };
 
 export default function Dropdown({ title = 'Choose', items, onChangeAction, activeDevice }: DropdownPropsTypes) {
+	console.log({activeDevice})
 	return (
 		<Menu as='div' className='relative inline-block text-left'>
 			<div>
 				<Menu.Button className='inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50'>
-					{activeDevice ? activeDevice?.label : title}
+					{/* {activeDevice ? activeDevice?.label : title} */}
+					{/* @ts-ignore */}
+					{items?.[0].label}
 					<ChevronDownIcon className='-mr-1 h-5 w-5 text-gray-400' aria-hidden='true' />
 				</Menu.Button>
 			</div>
